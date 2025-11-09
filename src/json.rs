c9a73bd3b09f19087399c4034ef9841142bd6d76
DerDrodt <d.drodt@gmx.de>
2025-11-09 13:48:42 +0100

    Add comma-suffix to name

--- src/json.rs
+++ src/json.rs
@@ -98,6 +98,8 @@ pub struct NameItem {
     pub dropping_particle: Option<String>,
     /// A name suffix like `"Jr., Ph.D."`.
     pub suffix: Option<String>,
+    /// Whether a comma should be added before the suffix, e.g., `Smith, Jr.`
+    pub comma_suffix: Option<bool>,
 }
 
 /// A name that doesn't necessarily follow the schema of a `NameItem`. May be
@@ -484,6 +486,7 @@ mod tests {
                 non_dropping_particle: None,
                 dropping_particle: None,
                 suffix: None,
+                comma_suffix: None,
             })]),
         );
         map.insert(
